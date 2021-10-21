136d2bc7579244df4db31c7578f75c719ca1c431
Daniel López <jdanoolss@gmail.com>
2021-10-21 16:15:50 -0500

    Implement BubbleMessage Component

---
+++ src/views/BubbleMessage.js
@@ -0,0 +1,50 @@
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar'
+import { styled } from '@mui/system';
+import { makeStyles } from '@mui/styles';
+import avatar_default from '../img/avatar_default.png';
+
+const useStyles = makeStyles((theme) => ({
+  bubblecolor:(props) => ({
+    backgroundColor: props.sentbyme 
+    ? theme.palette.bubble.main
+    : theme.palette.bubble.secondary,
+  })
+}));
+
+const BubbleMessageContainer = styled('div')(`
+  width: auto;
+  height: auto;
+  border-radius: 20px;
+  box-shadow: 0px 1.06px 1.06px rgba(0, 0, 0, 0.15);
+`);
+
+function BubbleMessage(props) {
+  const classes = useStyles(props);
+
+  const renderAvatar = () => {
+    if (props.isfirst) {
+      return <Avatar alt="Avatar" src={avatar_default} sx={{ width: 40, height: 40, mr: 10, ml: 10}} />;
+    }
+  }
+
+  return (
+    <Grid direction={ props.sentbyme ? "row": "row-reverse" }
+      container
+      mt={ props.isfirst ? 24 : 0 }
+      pl={ props.isfirst ? 0 : 58 }
+      pr={ props.isfirst ? 0 : 58 }
+      justifyContent="flex-start"
+      alignItems="flex-start" >
+      { renderAvatar() }
+      <BubbleMessageContainer { ...props } className={ classes.bubblecolor } sx={{ maxWidth: '55%', mb: 4, pt: 12, pb: 12, pl: 20, pr: 20 }} >
+        <Typography variant="caption" color="text.message" sx={{ mx: 'auto' }}>
+          { props.message }
+        </Typography>
+      </BubbleMessageContainer>
+    </Grid>
+  )
+}
+
+export default BubbleMessage;
--- src/views/User.js
+++ src/views/User.js
@@ -1,18 +1,29 @@
 import '../css/user.css';
 
 import Grid from '@mui/material/Grid';
+
 import Contact from "./Contact";
 import UserProfile from "./UserProfile";
-
+import BubbleMessage from "./BubbleMessage";
 
 function User() {
   return (
     <div className="User">
       <Grid container direction="row" className="main-container">
-        <Grid item lg={2} sx={{ bgcolor: 'background.default' }} container>
+        <Grid item lg={2} sx={{ bgcolor: 'background.secondary' }} container>
           <UserProfile />
         </Grid>
-        <Grid item lg={8} sx={{ bgcolor: 'primary.main' }} container>
+        <Grid item lg={8} sx={{ bgcolor: 'background.default' }} container>
+          <Grid sx={{ pl: 48, pr: 53, pt: 20 }} direction="column" container>
+            <BubbleMessage sentbyme={1} isfirst={1} message="Hola" />
+            <BubbleMessage sentbyme={0} isfirst={1} message="Hola" />
+            <BubbleMessage sentbyme={1} isfirst={1} message="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+            <BubbleMessage sentbyme={1} isfirst={0} message="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+            <BubbleMessage sentbyme={0} isfirst={1} message="It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." />
+            <BubbleMessage sentbyme={0} isfirst={0} message="It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." />
+            <BubbleMessage sentbyme={0} isfirst={0} message="It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." />
+            <BubbleMessage sentbyme={1} isfirst={1} message="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+          </Grid>
         </Grid>
         <Grid item lg={2} sx={{ bgcolor: 'background.secondary' }} container>
           <Contact />
